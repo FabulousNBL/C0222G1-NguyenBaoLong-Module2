dc0c0d90b7f6357d3864b3655877f6dc5665079c
FabulousNBL <nguyenblong2412@gmail.com>
2022-04-13 21:43:14 +0700

    upd

---
+++ src/_10_list/exercise/linked_list/MyLinkedListTest.java
@@ -0,0 +1,22 @@
+package _10_list.exercise.linked_list;
+
+public class MyLinkedListTest {
+    public static void main(String[] args) {
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+        myLinkedList.addFirst("Long");
+        myLinkedList.addLast("Hoàng");
+        myLinkedList.addFirst( "Hòa");
+        myLinkedList.addLast( "Nam");
+        myLinkedList.add(2,"Hương");
+//        myLinkedList.clear();
+//
+//        System.out.println(myLinkedList.get(4));
+//        myLinkedList.removeFirst();
+//        myLinkedList.removeLast();
+//        System.out.println(myLinkedList.contain("Loan"));
+//        System.out.println(myLinkedList.getLast());
+//        myLinkedList.clone().printList();
+
+
+    }
+}
